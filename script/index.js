3393c449689abee9a2bcc00c73a6ba7f52c07d95
sohanur28 <gsrahman28@gmail.com>
2026-02-11 20:27:17 +0600

    load words by level

--- script/index.js
+++ script/index.js
@@ -4,6 +4,27 @@ const loadLessons = () => {
         .then(json => displayLesson(json.data));
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWord(data.data))
+};
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    // wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        console.log(word)
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <p>Cat</p>
+        `;
+        wordContainer.append(card);
+    });
+};
+
 const displayLesson = (lessons) => {
     // 1. get the container & empty
     const levelContainer = document.getElementById("level-container");
@@ -11,10 +32,10 @@ const displayLesson = (lessons) => {
     // 2. get into every lessons
     for (let lesson of lessons) {
         // 3. create Element
-        console.log(lesson)
+        // console.log(lesson)
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-                <button class="btn btn-outline btn-primary">
+                <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
                 <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
                 </button>
         `;
